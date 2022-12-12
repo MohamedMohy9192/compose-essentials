e0affbaa65f64a6860762321a486ca438dd1448a
Mohamed Mohy <mohamed.mohy.info@gmail.com>
2022-12-12 04:53:26 +0200

    Create messages scrollable list with an expandable item

--- ComposeTutorial/app/src/main/java/com/example/android/composetutorial/MainActivity.kt
+++ ComposeTutorial/app/src/main/java/com/example/android/composetutorial/MainActivity.kt
@@ -1,35 +1,142 @@
 package com.example.android.composetutorial
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.android.composetutorial.ui.theme.ComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // The setContent block defines the activity's layout where composable functions are called.
         setContent {
-            
+            // Composable functions can only be called from other composable functions.
+            ComposeTutorialTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    Conversation(SampleData.conversationSample)
+                }
+            }
         }
     }
 }
 
+data class Message(val author: String, val body: String)
+
+//To make a function composable, add the @Composable
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MessageCard(msg: Message) {
+    // The Column function lets you arrange elements vertically.
+    // You can use Row to arrange items horizontally and Box to stack elements.
+
+    // Add padding around our message
+    Row(Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.profile_picture),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                // Set image size to 40 dp
+                .size(40.dp)
+                // Clip image to be shaped as a circle
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+        )
+
+        // Add a horizontal space between the image and the column
+        Spacer(modifier = Modifier.width(8.dp))
+
+        // We keep track if the message is expanded or not in this
+        // variable
+        var isExpanded by remember { mutableStateOf(false) }
+        // surfaceColor will be updated gradually from one color to the other
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
+        )
+
+        // We toggle the isExpanded variable when we click on this Column
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
+            // Add a vertical space between the author and message texts
+            Spacer(modifier = Modifier.height(4.dp))
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                // surfaceColor color will be changing gradually from primary to surface
+                color = surfaceColor,
+                // animateContentSize will change the Surface size gradually
+                modifier = Modifier
+                    .animateContentSize()
+                    .padding(1.dp)
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.body2
+                )
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+
 @Composable
-fun DefaultPreview() {
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        messages.map { message ->
+            item { MessageCard(message) }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation() {
     ComposeTutorialTheme {
-        Greeting("Android")
+        Conversation(SampleData.conversationSample)
+    }
+}
+
+// The @Preview annotation lets you preview your composable functions within Android Studio without
+// having to build and install the app to an Android device or emulator.
+// The annotation must be used on a composable function that does not
+// take in parameters.
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+@Composable
+fun PreviewMessageCard() {
+    ComposeTutorialTheme {
+        Surface {
+            MessageCard(
+                msg = Message("Colleague", "Take a look at Jetpack Compose, it's great!")
+            )
+        }
     }
 }
